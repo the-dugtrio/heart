a3353109dbe75b176933bff96d564a90dae74e93
周庆 <zhouqing01@corp.netease.com>
2019-08-07 15:52:11 +0800

    fix:解决次级路由修改时候照成的页面重新渲染

--- example/components/docs.jsx
+++ example/components/docs.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { HashRouter as Router, Redirect, Switch, Route, NavLink, Link } from 'react-router-dom';
+import connectRoute from './connectRoute';
 import Layout from './Layout';
 import isEmpty from "lodash/isEmpty";
 const routesMap = require('../map.json');
@@ -43,45 +44,51 @@ const getMenuUl=(filter, path)=>{
 
 const Menu = (props)=>{
     const {match} = props;
+    console.log(match);
     const filter = routesMap.filter(item=>item.path===match.path)[0];
-    console.log(filter);
     let ul = null
     if(filter.children){
         ul =    getMenuUl(filter.children, match.path);
+        //console.log('ul', ul);
     }
     return (<div>
         {ul}
     </div>);
 }
 
-const Test = ()=>{
+const Test1 = ()=>{
     return(<div>1</div>)
 }
 
+const Test2 = ()=>{
+    return(<div>2</div>)
+}
 const Content = (props)=>{
     const {match} = props;
     //console.log('Content', match)
     return (<div>
                 {match.url}
                 <Switch>
-                    <Route path={`${match.url}/index`} exact component={Test}></Route>
-                    <Route path={`${match.url}/index1`} exact component={Test}></Route>
-                    <Route path={`${match.url}/index2`} exact component={Test}></Route>
+                    <Route path={`${match.url}/badge`} exact component={Test1}></Route>
+                    <Route path={`${match.url}/installation`} exact component={Test2}></Route>
                 </Switch>
            </div>);
 }
 
 const Common = (props)=>{
+    console.log('props', props);
     return(<Layout menu={<Menu {...props}/>} content={<Content {...props}/>}></Layout>)
 }
+const CommonWrapper = connectRoute(Common);
+
 export default class App extends Component {
     render() {
         return (
             <Router>
                 <Switch>
-                    <Route path="/docs/guide" component={Common} />
-                    <Route path="/components" component={Common}/>
-                    <Route path="/resource" component={Common}  />
+                    <Route path="/docs/guide" component={CommonWrapper} />
+                    <Route path="/components" component={CommonWrapper}/>
+                    <Route path="/resource" component={CommonWrapper}  />
                     <Redirect to="/docs/guide" />
                 </Switch>
             </Router>
